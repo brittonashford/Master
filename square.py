9b07cd8a8e36af7b47363848321cefb17f0c1e02
brittonashford <brittonashford@users.noreply.github.com>
2017-12-31 13:52:25 -0700

    Add files via upload

---
+++ square.py
@@ -0,0 +1,11 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+    franklin = turtle.Turtle()
+    franklin.forward(100)
+
+    window.exitonclick()
+
+draw_square()
